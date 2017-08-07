6429cf20f669c7da809ec4e2f96b3e831ef7eae6
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-07 15:00:40 -0700

    add update component

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
@@ -13,6 +14,9 @@ export default Ember.Component.extend({
       if (confirm('Are you sure you want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
       }
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
     }
   }
 });
