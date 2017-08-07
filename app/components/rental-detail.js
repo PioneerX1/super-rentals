a8b49935eaf9d901e2736142951682d9b02f4346
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-07 16:57:23 -0700

    add application template for overall layout

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     delete(rental) {
+      console.log(rental);
       if (confirm('Are you sure you want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
       }
